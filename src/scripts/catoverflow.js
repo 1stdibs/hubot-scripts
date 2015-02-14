0046cd769d60cbd61be988c590111e13b52f4c3d
Shad Downey <shad@1stdibs.com>
2015-02-13 20:06:12 -0500

    Improvements to cat bomb efficiency

--- src/scripts/catoverflow.js
+++ src/scripts/catoverflow.js
@@ -2,48 +2,56 @@
 //   Random cat images from Cat Overflow
 //
 // Dependencies:
-//   None
+//   underscore
 //
 // Commands:
-//   hubot cat me
-//   hubot cat bomb (n)
+//   hubot cat me - Random cat gif from www.catoverflow.com
+//   hubot cat bomb (n) - n/5 random cat gifs from www.catoverflow.com
 //
 // Author:
 //   andromedado
 
-var max = 320;
+var _ = require('underscore');
+var util = require('util');
+var max = 338;
 
 module.exports = function(robot) {
 
-    function getSrc (callback) {
-        robot.http('http://catoverflow.com/api/query?limit=1&offset=' + (Math.ceil(Math.random() * max))).get()(function (err, res, body) {
+    function getSrcs (num, callback) {
+        num = num || 1;
+        url = util.format('http://catoverflow.com/api/query?limit=%s&offset=%s', num, (Math.ceil(Math.random() * (max - num + 1))));
+        robot.http(url).get()(function (err, res, body) {
             if (err) {
                 callback(err);
                 return;
             }
-            callback(null, [String(body).replace(/^\s+/, '').replace(/\s+$/, ''), '.png'].join('#'));
+            var urls = (body + '').replace(/^\s*|\s$/, '').split('\n');
+            var urlsToUse = _.map(urls, function (url) {
+                return url + '#.png';
+            });
+            callback(null, urlsToUse);
         });
     }
 
-    function getIt (msg) {
-        getSrc(function (err, src) {
+    function fetchAndSend (num, msg) {
+        getSrcs(num, function (err, srcs) {
             if (err) {
                 msg.send(':flushed: ' + err);
             } else {
-                msg.send(src);
+                _.map(srcs, function (src) {
+                    msg.send(src);
+                });
             }
         })
     }
 
     robot.respond(/cat bomb( (\d+))?/i, function (msg) {
         var num = Math.min(30, msg.match[2] || 5);
-        while (num--) {
-            getIt(msg);
-        }
+        fetchAndSend(num, msg);
     });
 
     robot.respond(/cat me/i, function (msg) {
-        getIt(msg);
+        fetchAndSend(1, msg);
     });
 
 };

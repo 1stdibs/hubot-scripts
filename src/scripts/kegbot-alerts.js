839e7335297395e7fe219a6177ff84690b0670db
Shad Downey <shad@1stdibs.com>
2015-05-21 19:03:43 -0400

    KegBot Alert Update

--- src/scripts/kegbot-alerts.js
+++ src/scripts/kegbot-alerts.js
@@ -36,11 +36,16 @@ module.exports = function(robot) {
             envelope.user.type = query.type;
         }
         try {
-            data = req.body;
+            data = req.body.data;
+            var who = data.user.display_name;
+            var drankMl = parseFloat(data.drink.volume_ml);
+            var drankOz = drankMl * 0.033814;
+            var drinkName = data.keg.beverage.name;
+            var msg = who + ' drank ' + (drankOz.toFixed(1)) + 'oz of ' + drinkName;
             console.log(data);
             console.log("Someone drank " + data.drink.volume_ml);
             console.log("There's " + data.keg.volume_ml_remain + " left");
-            //robot.messageRoom('#general', "data.drink.volume_ml");
+            robot.messageRoom('#general', msg);
             //robot.messageRoom('#general', "data.keg.volume_ml_remain");
         } catch (error) {
             console.log(error);
